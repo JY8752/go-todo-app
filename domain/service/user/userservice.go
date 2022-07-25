f1a72588966c2be115e56924faf8b9b7618521f6
JY8752 <j.y.8752@gmail.com>
2022-07-25 19:59:32 +0900

    mock

--- domain/service/user/userservice.go
+++ domain/service/user/userservice.go
@@ -6,8 +6,15 @@ import (
 	"todo-app/ent"
 )
 
+type UserRepository interface {
+	NewUserRepository(*ent.Client) *user.UserRepository
+	FindById(context.Context, int) (*ent.User, error)
+	FindAll(context.Context) ([]*ent.User, error)
+	Create(context.Context, string, string, int) (*ent.User, error)
+}
+
 type UserService struct {
-	userRepository *user.UserRepository
+	userRepository UserRepository
 }
 
 func (service UserService) Create(ctx context.Context, name, email string, age int) (*ent.User, error) {
